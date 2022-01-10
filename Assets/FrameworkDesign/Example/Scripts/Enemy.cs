1d482252fcf35382c44c202f01792444d2da5891
Haogehaojiu <graykaras@163.com>
2022-01-10 18:02:57 +0800

    2 and add .gitignore new file:   .gitignore new file:   Assets/FrameworkDesign/Example/Game.unity new file:   Assets/FrameworkDesign/Example/Scripts/Enemy.cs new file:   Assets/FrameworkDesign/Prefabs/SmallSquare.png new file:   Assets/FrameworkDesign/Resources/Font/Menlo.ttc new file:   ProjectSettings/AudioManager.asset new file:   ProjectSettings/AutoStreamingSettings.asset new file:   ProjectSettings/ClusterInputManager.asset new file:   ProjectSettings/DynamicsManager.asset new file:   ProjectSettings/EditorBuildSettings.asset new file:   ProjectSettings/EditorSettings.asset new file:   ProjectSettings/GraphicsSettings.asset new file:   ProjectSettings/InputManager.asset new file:   ProjectSettings/NavMeshAreas.asset new file:   ProjectSettings/PackageManagerSettings.asset new file:   ProjectSettings/Physics2DSettings.asset new file:   ProjectSettings/PresetManager.asset new file:   ProjectSettings/ProjectSettings.asset new file:   ProjectSettings/ProjectVersion.txt new file:   ProjectSettings/QualitySettings.asset new file:   ProjectSettings/TagManager.asset new file:   ProjectSettings/TimeManager.asset new file:   ProjectSettings/UnityConnectSettings.asset new file:   ProjectSettings/VFXManager.asset new file:   ProjectSettings/XRSettings.asset

---
+++ Assets/FrameworkDesign/Example/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace FrameworkDesign.Example
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject gamePassPanel;
+        private static int killedEnemyCount;
+        private void Awake()
+        {
+            if (gamePassPanel.activeSelf)
+            {
+                gamePassPanel.gameObject.SetActive(false);
+            }
+        }
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+            ++killedEnemyCount;
+            if (killedEnemyCount >= 10)
+            {
+                gamePassPanel.gameObject.SetActive(true);
+            }
+        }
+    }
+
+}
