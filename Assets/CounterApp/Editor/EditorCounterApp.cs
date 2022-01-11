11842ba12e935f9355432feaef540a5d0f120b72
Haogehaojiu <graykaras@163.com>
2022-01-11 21:25:48 +0800

    10 扩展EditorCounterApp 本节总结： 表现层到底层系统层用Command 底层系统层到表现层用委托或者事件（通知） 表现层是可以替换的 new file:   Assets/CounterApp/Editor/EditorCounterApp.cs

---
+++ Assets/CounterApp/Editor/EditorCounterApp.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+namespace CounterApp.Editor
+{
+    public class EditorCounterApp : EditorWindow
+    {
+        [MenuItem("EditorCounterApp/Open")]
+        static void Open()
+        {
+            var window = GetWindow<EditorCounterApp>();
+            window.position = new Rect(100, 100, 400, 600);
+            window.titleContent = new GUIContent(nameof(EditorCounterApp));
+            window.Show();
+        }
+        private void OnGUI()
+        {
+            if (GUILayout.Button("+"))
+            {
+                new AddCountCommand().Execute();
+            }
+            GUILayout.Label(CounterModel.Count.Value.ToString());
+            if (GUILayout.Button("-"))
+            {
+                new SubCountCommand().Execute();
+            }
+        }
+    }
+}
